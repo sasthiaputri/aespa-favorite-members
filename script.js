00bcdbe658855347748a139a93306d07a989ecfa
Sasthia Putri <thiaputrinaryesi24@gmail.com>
2025-09-04 16:15:20 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+
+    function changemember (member) {
+        switch (member) {
+            case "karina":
+                document.getElementById("img").src="img/karina.png";
+                document.getElementById("name").innerHTML="Karina"
+                break;
+            case "winter":
+                document.getElementById("img").src="img/winter.png";
+                document.getElementById("name").innerHTML="Winter"
+                break;
+            case "giselle":
+                document.getElementById("img").src="img/giselle.png";
+                document.getElementById("name").innerHTML="Giselle"
+                break;
+            case "ningning":
+                document.getElementById("img").src="img/ningning.png";
+                document.getElementById("name").innerHTML="Ning Ning"
+                break;
+        }
+    }
